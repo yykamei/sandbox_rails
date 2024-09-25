4127b842eb75c5f6b1ee0a86aeb4aba85785805b
Yutaka Kamei <kamei@yykamei.me>
2024-09-25 16:34:42 +0900

    Add User

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_05_235556) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_25_073250) do
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
