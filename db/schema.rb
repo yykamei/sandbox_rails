a698545aefbfd35ea8fba29056860a3d968d9466
Yutaka Kamei <kamei@yykamei.me>
2024-11-10 17:21:41 +0900

    Bump rails to 8.0.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_25_073505) do
+ActiveRecord::Schema[8.0].define(version: 2024_09_25_073505) do
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", null: false
